e0ac306ad93e63c336ec8c17311e614711c517ec
serg <serg@ukr.net>
2017-11-13 16:20:26 +0200

    update 2 to release

--- src/ua/com/testcms/actions/BaseServlet.java
+++ src/ua/com/testcms/actions/BaseServlet.java
@@ -20,6 +20,7 @@ public abstract class BaseServlet  extends SecureServlet {
  //test merge
  //reliase 1.0
  // update 1
+ // update 2
  
  protected static final Map ACTIONS_MAP = new HashMap();
  protected static final int ACTION_UNKNOWN = 0;
